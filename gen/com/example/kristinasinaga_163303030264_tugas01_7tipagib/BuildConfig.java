faaa988212ff68c17a5b79d0854b5ff6ee097882
lidyaaprillasiregar <lidyaaprilliasiregar@gmail.com>
2019-11-05 07:12:51 +0700

    Tugas 1 Pemograman Mobile II

---
+++ gen/com/example/kristinasinaga_163303030264_tugas01_7tipagib/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.kristinasinaga_163303030264_tugas01_7tipagib;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
